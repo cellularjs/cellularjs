ab6e0e21e82dacc65f2c0bcd1abd4f96c336c78f
great-elephant <hoangtv2101@gmail.com>
2021-12-16 19:03:43 +0700

    feat(di): allow to import exported module automatically

--- packages/di/src/container/funcs/addExportServicesAsProviders.func.ts
+++ packages/di/src/container/funcs/addExportServicesAsProviders.func.ts
@@ -1,16 +1,23 @@
 import { getModuleMeta } from '../../utils';
 import { ClassType, ExportableCnf } from '../../types';
-import { Container } from '../../';
+import { Container, ExtModuleMeta } from '../../';
 
 export async function addExportServicesAsProviders(this: Container, exports: ExportableCnf[] = []): Promise<void> {
   for (let i = 0; i < exports.length; i++) {
-    const provider = exports[i];
-    // TODO: prevent complicate nested ExtendModule
-    // if ((provider as ExtModuleMeta).extModule) throw;
+    const exportCnf = exports[i];
 
-    // ignore module class
-    if (getModuleMeta(provider as ClassType)) continue;
+    // TODO: add test for better TEST COVERAGE.
+    if ((<ExtModuleMeta>exportCnf).extModule) {
+      await this._addExtModule(<ExtModuleMeta>exportCnf);
+      continue;
+    }
 
-    await this.addProvider(provider as ClassType);
+    // TODO: add test for better TEST COVERAGE.
+    if (getModuleMeta(<ClassType>exportCnf)) {
+      await this.addModule(exportCnf);
+      continue;
+    }
+
+    await this.addProvider(<ClassType>exportCnf);
   }
 }
