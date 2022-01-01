71f7743f9d54a46876871e792159db6b229d5dae
great-elephant <hoangtv2101@gmail.com>
2022-01-01 23:20:16 +0700

    fix(cli): add .gitignore file when initializing project structure

--- packages/cli/src/task/create-project-structure.task.ts
+++ packages/cli/src/task/create-project-structure.task.ts
@@ -15,4 +15,10 @@ export function createProjectStructure(cwd: string, projectName: string) {
   const packageJson = fse.readJSONSync(packageJsonPath, { encoding: 'utf-8' });
   packageJson.name = projectName;
   fse.writeJSONSync(packageJsonPath, packageJson, { encoding: 'utf-8', spaces: 2 });
+
+  // https://stackoverflow.com/q/24976950
+  fse.moveSync(
+    path.resolve(projectRootPath, 'gitignore'),
+    path.resolve(projectRootPath, '.gitignore'),
+  );
 }
