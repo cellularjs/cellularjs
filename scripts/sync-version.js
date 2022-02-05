16ccac29b2c309f3227c00383e1d95b1a8337bab
great-elephant <hoangtv2101@gmail.com>
2022-02-05 15:15:58 +0700

    build: sync version

--- scripts/sync-version.js
+++ scripts/sync-version.js
@@ -7,32 +7,50 @@ const lernaConfig = fse.readJSONSync(path.resolve(
 ));
 
 const targetVersion = lernaConfig.version;
+const builtTemplatePackageJson = path.resolve(__dirname, '..', 'packages', 'cli', 'dist', 'templates', 'halo', 'package.json');
 const projectPaths = [
   path.resolve(__dirname, '..', 'packages', 'cli', 'src', 'templates', 'halo'),
   path.resolve(__dirname, '..', 'packages', 'net'),
+  path.resolve(__dirname, '..', 'packages', 'di'),
+  path.resolve(__dirname, '..', 'packages', 'cli'),
 ];
 
-projectPaths.forEach(projectPath => {
-  const packageJsonPath = path.resolve(projectPath, 'package.json');
+function syncPackageVersion(packageJsonPath) {
   const packageData = fse.readJSONSync(packageJsonPath, { encoding: 'utf-8' });
 
   packageData.dependencies && Object.keys(packageData.dependencies).forEach(key => {
     if (!key.startsWith('@cellularjs/')) {
       return;
     }
-  
-    packageData.dependencies[key] = targetVersion;
+
+    packageData.dependencies[key] = `^${targetVersion}`;
   });
 
   packageData.peerDependencies && Object.keys(packageData.peerDependencies).forEach(key => {
     if (!key.startsWith('@cellularjs/')) {
       return;
     }
-  
-    packageData.peerDependencies[key] = targetVersion;
+
+    packageData.peerDependencies[key] = `^${targetVersion}`;
   });
-  
+
+  packageData.devDependencies && Object.keys(packageData.devDependencies).forEach(key => {
+    if (!key.startsWith('@cellularjs/')) {
+      return;
+    }
+
+    packageData.devDependencies[key] = `^${targetVersion}`;
+  });
+
   fse.writeJSONSync(packageJsonPath, packageData, { encoding: 'utf-8', spaces: 2 });
-  
-  childProcess.execSync('git add ' + packageJsonPath);
+}
+
+syncPackageVersion(builtTemplatePackageJson);
+
+projectPaths.forEach(projectPath => {
+  const packageJsonPath = path.resolve(projectPath, 'package.json');
+  syncPackageVersion(packageJsonPath);
+
+  childProcess.execSync(`git add ${packageJsonPath}`);
 });
+
