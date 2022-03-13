f654202c32704aa929186f444a86369c931181c7
great-elephant <hoangtv2101@gmail.com>
2022-03-13 21:48:20 +0700

    feat(cli): support run dev/build with multilpe entries #9

--- packages/cli/src/cmd/build.cmd.ts
+++ packages/cli/src/cmd/build.cmd.ts
@@ -4,9 +4,9 @@ import chalk from 'chalk';
 import { getCellularConfig } from '../helper/get-cellular-config';
 import { getWebpackConfig } from '../helper/get-webpack-config';
 
-function handleBuildCmd(entryName: string) {
+function handleBuildCmd(entries: string[]) {
   const cellularCnf = getCellularConfig();
-  const webpackConfig = getWebpackConfig(cellularCnf, entryName, 'production');
+  const webpackConfig = getWebpackConfig(cellularCnf, entries, 'production');
 
   webpack(webpackConfig, (err, stats) => {
     if (err || stats.hasErrors()) {
@@ -17,11 +17,11 @@ function handleBuildCmd(entryName: string) {
 
 export const buildCmd = new Command('build')
   .option(
-    '-e, --entry <name>',
+    '-e, --entry [name...]',
     'Entry name, it is declared inside file "cellular.ts"',
   )
   .action((args) => {
-    if (!args.entry) {
+    if (args.entry.length === 0) {
       return console.log(
         chalk.cyanBright(
           'Missing entry name\n(Correct example: cellular build -e http)',
