33754f491db4a35601fc71387553c22e01e8e184
great-elephant <hoangtv2101@gmail.com>
2022-03-15 01:52:45 +0700

    feat(net): add service name into referer header of IRQ
    Close: #11

--- packages/net/test/fixture/app/ims/events/delegate-unlock-account.event.ts
+++ packages/net/test/fixture/app/ims/events/delegate-unlock-account.event.ts
@@ -1,20 +1,14 @@
-import {
-  Service,
-  ServiceHandler,
-  IRQ,
-  CellContext,
-  send,
-} from '../../../../../src';
+import { Service, ServiceHandler, IRQ, send } from '../../../../../src';
 
 @Service({ scope: 'publish' })
 export class DelegateUnlockAccount implements ServiceHandler {
-  constructor(private ctx: CellContext) {}
+  constructor(private irq: IRQ) {}
 
   handle() {
     const delegateUnlockAccountIrq = new IRQ({ to: 'Auth:UnlockAccount' });
 
     return send(
-      delegateUnlockAccountIrq.withHeaderItem('referer', this.ctx.cellName),
+      delegateUnlockAccountIrq.withHeaderItem('referer', this.irq.header.to),
       { throwOriginalError: true },
     );
   }
--- packages/net/test/fixture/cells/user/events/delegate-cache-html.event.ts
+++ packages/net/test/fixture/cells/user/events/delegate-cache-html.event.ts
@@ -1,19 +1,13 @@
-import {
-  Service,
-  ServiceHandler,
-  send,
-  IRQ,
-  CellContext,
-} from '../../../../../src';
+import { Service, ServiceHandler, send, IRQ } from '../../../../../src';
 
 @Service({ scope: 'publish' })
 export class DelegateCacheHtml implements ServiceHandler {
-  constructor(private ctx: CellContext) {}
+  constructor(private irq: IRQ) {}
 
   handle() {
     const cacheHtmlIrq = new IRQ({ to: 'IMS:CacheHtml' });
 
-    return send(cacheHtmlIrq.withHeaderItem('referer', this.ctx.cellName), {
+    return send(cacheHtmlIrq.withHeaderItem('referer', this.irq.header.to), {
       throwOriginalError: true,
     });
   }
