12894ee0c147a801deecc95adb9ca125054ec4fd
great-elephant <hoangtv2101@gmail.com>
2022-07-16 21:44:34 +0700

    fix(cli): show error information when installing packages #46

--- packages/cli/src/task/install-package.task.ts
+++ packages/cli/src/task/install-package.task.ts
@@ -1,23 +1,18 @@
 import { spawn } from 'child_process';
-import { Spinner } from 'clui';
 import logSymbols from 'log-symbols';
 import { PackageInstallCommands, PackageManager } from '../const';
 
 export function installPackage(cwd: string, packageManager: PackageManager) {
   const installCmd = PackageInstallCommands[packageManager];
-  const spinner = new Spinner('Installing packages...');
-
-  spinner.start();
 
   return new Promise((resolve, reject) => {
     const spawnProcess = spawn(installCmd, {
       cwd,
       shell: true,
+      stdio: 'inherit',
     });
 
     spawnProcess.on('exit', (code) => {
-      spinner.stop();
-
       if (code === 0) {
         return resolve(code);
       }
