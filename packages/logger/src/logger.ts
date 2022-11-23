8416b2db7008394f6e23877215b238ee8305dac2
great-elephant <hoangtv2101@gmail.com>
2022-11-23 23:08:44 +0700

    docs(logger): update comment

--- packages/logger/src/logger.ts
+++ packages/logger/src/logger.ts
@@ -4,14 +4,15 @@ import { LogMessage, LogMeta } from './logging';
 export interface Logger {
   /**
    * Get source(~operation) name.
+   *
    * @since 0.12.0
    */
   getSource(): string | undefined;
 
   /**
    * Create new logger instance that inherit these below properites from its parent:
-   * - Meta.
-   * - Log level.
+   * - Source name(If no source name is provided, it will use source name from parent logger).
+   * - Meta(It will combine with parent logger meta to create new meta).
    *
    * @since 0.12.0
    */
@@ -26,6 +27,8 @@ export interface Logger {
   setLogLevel(level: LogLevel): void;
 
   /**
+   * Get log level of current logger instance.
+   *
    * @since 0.12.0
    */
   getLogLevel(): LogLevel;
@@ -36,57 +39,47 @@ export interface Logger {
   setMeta(k: string, v: any): void;
 
   /**
+   * Get meta by key. If the key is not provided, it will return all meta.
+   *
    * @since 0.12.0
    */
   getMeta(k?: string): any;
 
   /**
-   * "A log level used for events considered to be useful during software debugging
-   * when more granular information is needed." - sematext.com
-   *
    * @since 0.12.0
    */
   debug(message: LogMessage, meta?: LogMeta): void;
 
   /**
-   * "An event happened, the event is purely informative and can be ignored during
-   * normal operations." - sematext.com
-   *
    * @since 0.12.0
    */
   info(message: LogMessage, meta?: LogMeta): void;
 
   /**
-   * Same as warn("Unexpected behavior happened inside the application, but it is continuing
-   * its work and the key business features are operating as expected." - sematext.com)
-   *
    * @since 0.12.0
    */
   warn(message: LogMessage, meta?: LogMeta): void;
 
   /**
-   * "One or more functionalities are not working, preventing some functionalities
-   * from working correctly." - sematext.com
-   *
    * @since 0.12.0
    */
   error(message: LogMessage, meta?: LogMeta): void;
 
   /**
-   * "One or more key business functionalities are not working and the whole system
-   * doesn’t fulfill the business functionalities." - sematext.com
-   *
    * @since 0.12.0
    */
   fatal(message: LogMessage, meta?: LogMeta): void;
 
   /**
+   * _Usage:_
    * ```ts
-   * // Usage:
    * import { LogLevel } from '@cellularjs/logger';
    *
    * logInstance.log(LogLevel.INFO, 'We have just did something');
-   * // same as: logInstance.info('We have just did something');
+   *
+   * // equal to:
+   *
+   * logInstance.info('We have just did something');
    * ```
    *
    * @since 0.12.0
