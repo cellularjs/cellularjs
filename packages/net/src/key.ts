7686fe083672c45e07d6a693b41380dba32fd31e
great-elephant <hoangtv2101@gmail.com>
2023-01-01 22:40:11 +0700

    chore(net): add version and document link

--- packages/net/src/key.ts
+++ packages/net/src/key.ts
@@ -8,4 +8,8 @@ export const CLL_CELL_OPTS = 'cll:net:cell:opts';
  */
 export const CLL_SERVICE_OPTS = 'cll:net:service:opts';
 
+/**
+ * @see https://cellularjs.com/docs/foundation/net/virtual-network#13-driver
+ * @since 0.1.0
+ */
 export const LOCAL_DRIVER = 'local';
--- packages/net/src/type.ts
+++ packages/net/src/type.ts
@@ -3,16 +3,26 @@ import { CellContext } from './';
 
 type ClassType<T> = { new (...args: any[]): T };
 
+/**
+ * @since 0.1.0
+ */
 export enum ServiceScopeMap {
   publish = 1,
   space = 2,
   private = 4,
 }
 
+/**
+ * @since 0.1.0
+ * @see https://cellularjs.com/docs/foundation/net/service#21-scope
+ */
 export type ServiceScope = keyof typeof ServiceScopeMap;
 
 /**
  * Service handler meta data.
+ *
+ * @since 0.1.0
+ * @see https://cellularjs.com/docs/foundation/net/service#2-service-decorator
  */
 export interface ServiceMeta {
   /**
@@ -24,6 +34,9 @@ export interface ServiceMeta {
    * - "private": limit access to owner cell only.
    *
    * *By default, the scope value is "space".*
+   *
+   * @since 0.1.0
+   * @see https://cellularjs.com/docs/foundation/net/service#21-scope
    */
   scope?: ServiceScope;
 }
@@ -32,17 +45,31 @@ export interface AjustedServiceMeta {
   scope: ServiceScopeMap;
 }
 
+/**
+ * @since 0.1.0
+ * @see https://cellularjs.com/docs/foundation/net/cell#21-providers
+ */
 export type CellProviderConfig = GenericProvider | string;
 
+/**
+ * @see https://cellularjs.com/docs/foundation/net/cell#2-cell-decorator
+ * @since 0.1.0
+ */
 export interface CellMeta {
   /**
    * It define set of providers for current cell.
+   *
+   * @see https://cellularjs.com/docs/foundation/net/cell#21-providers
+   * @since 0.1.0
    */
   providers?: CellProviderConfig[];
 
   /**
    * Compatible with @cellularjs/di, it define set of modules that will be imported
    * into current cell.
+   *
+   * @see https://cellularjs.com/docs/foundation/net/cell#22-imports
+   * @since 0.1.0
    */
   imports?: ImportableCnf[];
 
@@ -65,20 +92,35 @@ export interface CellMeta {
    * ```
    *
    * _CellularJS support many type but it is better to use a consistent type._
+   *
+   * @see https://cellularjs.com/docs/foundation/net/cell#23-listen
+   * @since 0.1.0
    */
   listen: string | { [serviceName: string]: ClassType<ServiceHandler> };
 }
 
 /**
  * @see https://cellularjs.com/docs/foundation/net/internal-message#211-to
+ * @since 0.3.0
  */
 export type ToTargetHeader = `${string}:${string}`;
 
+/**
+ * @see https://cellularjs.com/docs/foundation/net/internal-message#21-request-header
+ * @since 0.1.0
+ */
 export type IrqHeader = {
+  /**
+   * @see https://cellularjs.com/docs/foundation/net/internal-message#211-to
+   * @since 0.1.0
+   */
   to?: ToTargetHeader;
 
   /**
    * The name of "cell:service" that this request come from.
+   *
+   * @see https://cellularjs.com/docs/foundation/net/internal-message#212-referer
+   * @since 0.1.0
    */
   referer?: ToTargetHeader;
 
@@ -86,12 +128,21 @@ export type IrqHeader = {
 };
 
 export type IrsHeader = {
+  /**
+   * IRS status is similar to HTTP status code.
+   *
+   * @see https://cellularjs.com/docs/foundation/net/internal-message#311-status
+   * @since 0.1.0
+   */
   status?: number;
   [key: string]: any;
 };
 
 /**
  * Cell service handler.
+ *
+ * @see https://cellularjs.com/docs/foundation/net/service#3-service-handler
+ * @since 0.1.0
  */
 export interface ServiceHandler {
   handle(): any | Promise<any>;
@@ -100,6 +151,9 @@ export interface ServiceHandler {
 /**
  * Cell config contain cell infomation. You can get a cell config by invoking
  * `getResolvedCell('cell name')`.
+ *
+ * @see https://cellularjs.com/docs/foundation/net/virtual-network/#1-network-configuration
+ * @since 0.1.0
  */
 export interface CellConfig {
   /**
@@ -108,6 +162,9 @@ export interface CellConfig {
    *
    * **TIP**: *For better performance when comparing 2 cell name, let use `cellId`
    * (call `getResolvedCell(cellName)` to get `cellId`).*
+   *
+   * @see https://cellularjs.com/docs/foundation/net/virtual-network#11-name
+   * @since 0.1.0
    */
   name: string;
 
@@ -117,6 +174,9 @@ export interface CellConfig {
    *
    * **TIP**: *For better performance when comparing 2 space, let use `spaceId`
    * (call `getResolvedCell(cellName)` to get `spaceId`).*
+   *
+   * @see https://cellularjs.com/docs/foundation/net/virtual-network#12-space
+   * @since 0.1.0
    */
   space?: string;
 
@@ -138,6 +198,9 @@ export interface CellConfig {
    *   any: AuthAny",
    * }
    * ```
+   *
+   * @see https://cellularjs.com/docs/foundation/net/virtual-network#13-driver
+   * @since 0.1.0
    */
   driver:
     | {
@@ -151,40 +214,79 @@ export interface CellConfig {
    * more custom data with this property.
    *
    * You can get your customData by calling `getResolvedCell(cellNamme)`.
+   *
+   * @since 0.1.0
    */
   customData?: { [key: string]: any };
 }
 
 /**
  * If you have a cell, you have a network. If you have many cells, you have a big network.
+ *
+ * @see https://cellularjs.com/docs/foundation/net/virtual-network#11-name
+ * @since 0.1.0
  */
 export type NetworkConfig = Array<CellConfig>;
 
 export type ServiceHandlerMap = Map<string, ClassType<ServiceHandler>>;
 
+/**
+ * @since 0.1.0
+ */
 export interface ResolvedDriver {
+  /**
+   * @since 0.1.0
+   */
   container: Container;
+
+  /**
+   * @since 0.1.0
+   */
   listener: ServiceHandlerMap;
 }
 
+/**
+ * @since 0.1.0
+ */
 export interface ResolvedCell {
   /**
    * `cellId` is short version of `CellConfig.name`. It is usefull for task
    * like cell comparation.
+   *
+   * @since 0.1.0
    */
   cellId: number;
 
   /**
    * `spaceId` is short version of `CellConfig.space`. It is usefull for task
    * like space comparation.
+   *
+   * @see https://cellularjs.com/docs/foundation/net/transporter#2-using-specific-driver
+   * @since 0.1.0
    */
   spaceId: number;
 
+  /**
+   * @see https://cellularjs.com/docs/foundation/net/virtual-network/#1-network-configuration
+   * @since 0.1.0
+   */
   cellConfig: CellConfig;
 
+  /**
+   * Technically, a class that is decorated by `@Cell` decorator is just a driver.
+   *
+   * @see https://cellularjs.com/docs/foundation/net/virtual-network#13-driver
+   * @since 0.1.0
+   */
   drivers: Map<string, ResolvedDriver>;
 
+  /**
+   * @since 0.1.0
+   */
   cellContext: CellContext;
 }
 
+/**
+ * @since 0.1.0
+ */
 export type ServiceHandlerClass = { new (...args: any[]): ServiceHandler };
