258a9e4cf2f3fcd6a7e0e498b1832b32fb874886
great-elephant <hoangtv2101@gmail.com>
2023-11-09 04:01:32 +0700

    feat(cli): exclude __tests__, *.spec.ts, *.test.ts when using auto loading

--- packages/cli/src/const/index.ts
+++ packages/cli/src/const/index.ts
@@ -17,7 +17,12 @@ export const cellularConfigFile = 'cellular.ts';
 
 /**
  * Eg:
- * test.ts => OK
- * test.d.ts => NO
+ * file.ts => Pass
+ * __tests__/file.spec.ts => Fail
+ * __tests__/sub/file.ts => Fail
+ * file.spec.ts => Fail
+ * file.test.ts => Fail
+ * file.d.ts => Fail
  */
-export const TS_FILE_REGEX_STR = '/(^.?|.[^d]|[^.]d|[^.][^d]).ts$/';
+export const TS_FILE_REGEX_STR =
+  '/^(?!.*(__tests__\\/.*).ts$)(?!.*(\\.spec|\\.test|\\.d)\\.ts$).*\\.ts$/';
