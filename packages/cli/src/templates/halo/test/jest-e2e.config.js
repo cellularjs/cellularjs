c8291f2f97ee27a97f4f314318c17e73160dea70
Great Elephant <hoangtv2101@gmail.com>
2024-03-16 16:59:26 +0700

    feat(cli): config code coverage report for e2e testing (#135)

--- packages/cli/src/templates/halo/test/jest-e2e.config.js
+++ packages/cli/src/templates/halo/test/jest-e2e.config.js
@@ -1,8 +1,18 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  rootDir: '.',
+  rootDir: '..',
 
   testEnvironment: 'node',
+  testMatch: ['<rootDir>/test/**/*.spec.ts'],
+
+  coverageProvider: 'v8',
+  coverageDirectory: './coverage-e2e',
+  collectCoverageFrom: [
+    'src/**/*.{ts,js}',
+    '!src/**/*.{spec,test}.{ts,js}',
+    '!**/__tests__/**',
+    '!**/node_modules/**',
+  ],
 
   // Ref: https://stackoverflow.com/a/51174924
   moduleDirectories: ['node_modules', 'src'],
@@ -11,10 +21,8 @@ module.exports = {
     // Ref: https://kulshekhar.github.io/ts-jest/docs/getting-started/options/isolatedModules/#performance
     '^.+\\.ts?$': ['ts-jest', { isolatedModules: true }],
   },
-
-  // globalSetup: './setup.ts',
-  // globalTeardown: './teardown.ts',
-  setupFilesAfterEnv: ['./setup.ts'],
+  
+  setupFilesAfterEnv: ['./test/setup.ts'],
 
   silent: true,
 };
