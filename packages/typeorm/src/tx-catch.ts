21857171705092e1f650bfebd15455fda157d8d0
Great Elephant <hoangtv2101@gmail.com>
2024-04-11 00:17:47 +0700

    feat(typeorm): setDefaultCatch, callback is required (#138)

--- packages/typeorm/src/tx-catch.ts
+++ packages/typeorm/src/tx-catch.ts
@@ -18,7 +18,34 @@ export type TransactionalCatch = (opts: CatchOpts) => Promise<any>;
 
 let _defaultCatch: TransactionalCatch;
 
-export const setDefaultCatch = function (defaultCatch?: TransactionalCatch) {
+/**
+ * Set default error handler when using `Transactional` decorator.
+ *
+ * Example:
+ * ```ts
+ * // global.ts
+ * import { TransactionalCatch, setDefaultCatch } from '@cellularjs/typeorm';
+ *
+ * const MAX_RETRY = 1;
+ *
+ * const txCatch: TransactionalCatch = async ({ error, service, ctx }) => {
+ *  // Ref: https://www.postgresql.org/docs/current/mvcc-serialization-failure-handling.html
+ *  if (error?.code !== '40001') throw error;
+ *
+ *  ctx.try ||= 0;
+ *  ctx.try++;
+ *
+ *  if (ctx.try > MAX_RETRY) throw error;
+ *
+ *  return await service.handle();
+ * }
+ *
+ * setDefaultCatch(txCatch);
+ * ```
+ * @since 0.19.0
+ * @see https://cellularjs.com/docs/how-to%20wiki/typeorm#42-error-handling
+ */
+export const setDefaultCatch = function (defaultCatch: TransactionalCatch) {
   if (_defaultCatch) {
     throw new Error('Default catch already exist!');
   }
@@ -27,14 +54,14 @@ export const setDefaultCatch = function (defaultCatch?: TransactionalCatch) {
 };
 
 /**
- * For internal only!
+ * For internal usage only!
  */
 export const clearDefaultCatch = function () {
   _defaultCatch = null;
 };
 
 /**
- * For internal only!
+ * For internal usage only!
  */
 export const getDefaultCatch = function () {
   return _defaultCatch;
